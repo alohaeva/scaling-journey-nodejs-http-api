e913748961f631e28bbc60604f82356ce37a879d
Vladislav Evstigneev <vladislav.evstigneev@carrier.com>
2024-01-21 19:10:25 +0400

    add utils

--- README.md
+++ README.md
@@ -1,18 +1,1 @@
-[x] typescript
-
-[x] lint-staged
-
-[x] prettier
-
-[x] eslint
-
-[x] nodemon
-
-[x] jest
-
-[X] http-server
-
-[x] logger
-
-set up eslint rules
-set up tsconfig rules
+# NodeJS Project Boilerplate
--- src/utils/parse/decodeJwtString.ts
+++ src/utils/parse/decodeJwtString.ts
@@ -1,17 +1,22 @@
-function base64urlDecode(str: string) {
-  return Buffer.from(base64urlUnescape(str), 'base64').toString();
-}
+import { toNormalizedError } from '../normalError';
+import { logger } from '../../logger';
 
-function base64urlUnescape(str: string) {
+const base64urlUnescape = (str: string) => {
   str += Array(5 - (str.length % 4)).join('=');
   return str.replace(/-/g, '+').replace(/_/g, '/');
-}
+};
+
+const base64urlDecode = (str: string) => {
+  return Buffer.from(base64urlUnescape(str), 'base64').toString();
+};
 
 export const decodeJwtString = (jwtString: string) => {
   const segments = jwtString.split('.');
 
   if (segments.length !== 3) {
-    throw new Error('Not enough or too many segments');
+    const error = toNormalizedError(new Error('Not enough or too many segments'));
+
+    logger.error(error);
   }
 
   const headerSeg = segments[0];
