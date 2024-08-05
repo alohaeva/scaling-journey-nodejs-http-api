e56c16cf59699087beeceb57d8d963bb495f73c7
Vladislav Evstigneev <vladislav.evstigneev@carrier.com>
2024-08-05 18:15:27 +0400

    feat(api): update api hello route function

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,13 +1,14 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { StatusCode } from 'status-code-enum';
 
 import { helloRoute } from '../../routes/v1/index.ts';
 import { sendResponse } from '../helpers/sendResponse.ts';
+import { validationMiddleware } from '../../validators/validationMiddleware.ts';
 
 const apiV1Router = Router();
 
-apiV1Router.get(helloRoute.path, helloRoute.payloadValidator, (req, res) => {
-  const result = helloRoute.handler(req.url);
+apiV1Router.post(helloRoute.path, validationMiddleware(helloRoute.payloadValidator), (_: Request, res: Response) => {
+  const result = helloRoute.handler(res.locals.payload);
 
   return sendResponse(res, {
     result,
