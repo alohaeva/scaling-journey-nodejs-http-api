de7292fd508a9099401071ea9c34e6281950d55b
Vladislav Evstigneev <vladislav.evstigneev@carrier.com>
2024-08-05 18:14:19 +0400

    feat(routes): update configuration for hello route

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,11 +1,10 @@
 import { HelloService } from '../../services/HelloService.ts';
+import { validateHelloPayload } from '../../validators/index.ts';
+
+const helloService = new HelloService();
 
 export const helloRoute = {
   path: '/hello',
-  payloadValidator: () => {},
-  handler: (message: string) => {
-    const helloService = new HelloService();
-
-    return helloService.handler(message);
-  },
+  payloadValidator: validateHelloPayload,
+  handler: helloService.handler,
 };
